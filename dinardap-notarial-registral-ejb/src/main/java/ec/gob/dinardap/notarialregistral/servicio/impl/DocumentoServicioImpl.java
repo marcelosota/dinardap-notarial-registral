fd25e61a568399fd6a3dacf0a6fac22281ae4585
Marcelo Muñoz <leonardo.munoz@dinardap.gob.ec>
2020-06-12 12:23:53 -0500

    Corrección dependencias seguridades y geografico

--- dinardap-notarial-registral-ejb/src/main/java/ec/gob/dinardap/notarialregistral/servicio/impl/DocumentoServicioImpl.java
+++ dinardap-notarial-registral-ejb/src/main/java/ec/gob/dinardap/notarialregistral/servicio/impl/DocumentoServicioImpl.java
@@ -1,9 +1,13 @@
 package ec.gob.dinardap.notarialregistral.servicio.impl;
 
 
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 import javax.ejb.EJB;
 import javax.ejb.Stateless;
 
+import ec.gob.dinardap.notarialregistral.constante.ParametroEnum;
 import ec.gob.dinardap.notarialregistral.dao.DocumentoDao;
 import ec.gob.dinardap.notarialregistral.dto.SftpDto;
 import ec.gob.dinardap.notarialregistral.modelo.Documento;
@@ -12,10 +16,8 @@ import ec.gob.dinardap.persistence.dao.GenericDao;
 import ec.gob.dinardap.persistence.servicio.impl.GenericServiceImpl;
 import ec.gob.dinardap.seguridad.servicio.ParametroServicio;
 import ec.gob.dinardap.sftp.exception.FtpException;
-//import ec.gob.dinardap.sftp.util.CredencialesSFTP;
+import ec.gob.dinardap.sftp.util.CredencialesSFTP;
 import ec.gob.dinardap.sftp.util.GestionSFTP;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 
 @Stateless(name = "DocumentoServicio")
 public class DocumentoServicioImpl extends GenericServiceImpl<Documento, Long> implements DocumentoServicio {
@@ -40,23 +42,22 @@ public class DocumentoServicioImpl extends GenericServiceImpl<Documento, Long> i
 
     @Override
     public byte[] descargarArchivo(SftpDto sftpDto) {
-        /*try {
+        try {
             sftpDto.setCredencialesSFTP(setCredencialesSftp(sftpDto.getCredencialesSFTP()));
             return GestionSFTP.descargarArchivo(sftpDto.getCredencialesSFTP());
         } catch (FtpException ex) {
             Logger.getLogger(DocumentoServicioImpl.class.getName()).log(Level.SEVERE, null, ex);
             return null;
-        }*/
-    	return null;
+        }
     }
 
-    /*private CredencialesSFTP setCredencialesSftp(CredencialesSFTP credencialesSFTP) {
+    private CredencialesSFTP setCredencialesSftp(CredencialesSFTP credencialesSFTP) {
         credencialesSFTP.setHost(parametroServicio.findByPk(ParametroEnum.SERVIDOR_SFTP.name()).getValor());
         credencialesSFTP.setPuerto(Integer.parseInt(parametroServicio.findByPk(ParametroEnum.PUERTO_SFTP.name()).getValor()));
         credencialesSFTP.setUsuario(parametroServicio.findByPk(ParametroEnum.SFTP_USUARIO_NOTARIAL_REGISTRAL.name()).getValor());
         credencialesSFTP.setContrasena(parametroServicio.findByPk(ParametroEnum.SFTP_CONTRASENA_NOTARIAL_REGISTRAL.name()).getValor());
         return credencialesSFTP;
-    }*/
+    }
 
     @SuppressWarnings("unused")
 	private void guardarArchivo(SftpDto sftpDto) {
